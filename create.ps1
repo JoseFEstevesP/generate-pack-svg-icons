b9532e4a7e9109e4b6913cdc21ad38b72fb7e431
José Esteves <joseesteves199930@gmail.com>
2024-12-30 10:13:11 -0400

    Agregar opción para especificar la ruta del archivo de salida y verificar si ya existe antes de sobrescribir

--- create.ps1
+++ create.ps1
@@ -2,7 +2,20 @@ param (
     [string]$inputFolder
 )
 
-$outputFile = "$inputFolder.svg"
+# Solicitar la ruta del archivo de salida
+$outputDirectory = Read-Host "Ingrese la ruta completa donde se guardará el archivo (sin incluir el nombre del archivo)"
+
+# Construir la ruta del archivo de salida usando el nombre de la carpeta y la extensión .svg
+$outputFile = Join-Path -Path $outputDirectory -ChildPath "$($inputFolder).svg"
+
+# Verificar si el archivo ya existe y preguntar si desea reemplazarlo
+if (Test-Path $outputFile) {
+    $respuesta = Read-Host "El archivo ya existe. ¿Desea reemplazarlo? (s/n)"
+    if ($respuesta -ne 's') {
+        Write-Host "Operación cancelada."
+        exit
+    }
+}
 
 # Crear el archivo de salida y escribir el encabezado SVG
 @"
@@ -16,8 +29,7 @@ Get-ChildItem -Path $inputFolder -Filter *.svg | ForEach-Object {
     $content = Get-Content -Path $_.FullName
     
     # Eliminar atributos width y height
-    $content = $content -replace 'width="[^"]*"', ''
-    $content = $content -replace 'height="[^"]*"', ''
+    $content = $content -replace 'width="[^"]*"\s*|height="[^"]*"\s*', ''
     
     # Reemplazar xmlns y svg por symbol
     $content = $content -replace 'xmlns="http://www.w3.org/2000/svg"', ''
@@ -29,3 +41,5 @@ Get-ChildItem -Path $inputFolder -Filter *.svg | ForEach-Object {
 
 # Escribir el cierre del archivo SVG
 '</svg>' | Out-File -FilePath $outputFile -Append -Encoding utf8
+
+Write-Host "El archivo SVG ha sido creado exitosamente en: $outputFile"
