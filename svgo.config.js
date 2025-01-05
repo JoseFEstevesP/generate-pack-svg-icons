9bcca118cb8daf46abfd830c94fb94b9dd1abd17
jose Esteves <joseesteves199930@gmail.com>
2025-01-05 02:06:11 -0400

    configuracion de svgo

---
+++ svgo.config.js
@@ -0,0 +1,16 @@
+export default {
+	plugins: [
+		{
+			name: 'removeViewBox',
+			active: false,
+		},
+		{
+			name: 'removeDimensions',
+			active: true,
+		},
+		{
+			name: 'cleanupIds',
+			active: true,
+		},
+	]
+};
